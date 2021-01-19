132c24dc35480192467eb4bf173ad00a3f1e46be
alexlopotov <alexlopotov@gmail.com>
2021-01-19 16:23:49 +0300

    Files

---
+++ Miles/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        double ticket = 15000.50;
+        int miles = (int) ticket / 20;
+        System.out.println(miles);
+    }
+}
